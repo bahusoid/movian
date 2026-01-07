9b392b72630e0f070397a3b681c88c4783e04887
Roman Artiukhin <bahusdrive@gmail.com>
2026-01-07 13:42:27 +0200

    android: minimize  app instead of returning to Android HOME screen

--- android/src/com/lonelycoder/mediaplayer/GLWActivity.java
+++ android/src/com/lonelycoder/mediaplayer/GLWActivity.java
@@ -231,10 +231,7 @@ public class GLWActivity extends Activity implements VideoRendererProvider {
     public void sysHome() {
         runOnUiThread(new Runnable() {
                 public void run() {
-                    Intent startMain = new Intent(Intent.ACTION_MAIN);
-                    startMain.addCategory(Intent.CATEGORY_HOME);
-                    startMain.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                    startActivity(startMain);
+                    moveTaskToBack(true);
                 }
             });
     }
