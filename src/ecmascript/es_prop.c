2a4c43bee3d45eb990c4e54c2bbd35d608683a02
Roman Artiukhin <bahusdrive@gmail.com>
2026-06-20 11:38:06 +0300

    Revert "WIP. Expose follow needed for history plugin"
    This reverts commit 377dc8326e9d998c23cd1791c06621940c8c5c64.

--- src/ecmascript/es_prop.c
+++ src/ecmascript/es_prop.c
@@ -1091,19 +1091,6 @@ es_prop_node_filter_del_pred(duk_context *ctx)
 }
 
 
-/**
- * Follow symlinks to originator
- */
-static int
-es_prop_follow_duk(duk_context *ctx)
-{
-  prop_t *p = es_stprop_get(ctx, 0);
-  prop_t *f = prop_follow(p);
-  es_push_native_obj(ctx, &es_native_prop, f);
-  return 1;
-}
-
-
 static const duk_function_list_entry fnlist_prop[] = {
 
   { "print",               es_prop_print_duk,             1 },
@@ -1143,7 +1130,7 @@ static const duk_function_list_entry fnlist_prop[] = {
   { "nodeFilterAddPred",   es_prop_node_filter_add_pred,  6 },
   { "nodeFilterDelPred",   es_prop_node_filter_del_pred,  2 },
 
-  { "follow",              es_prop_follow_duk,            1 },
+
 
   { NULL, NULL, 0}
 };
